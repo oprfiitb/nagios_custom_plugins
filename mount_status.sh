2f0fa922c7a47cd59452a0e04d86b13e8922474c
sanman <sanmansabane0@gmail.com>
2016-09-29 20:12:58 +0530

    Debug capability for mount_status script too.
    We need more details to fix problems

--- mount_status.sh
+++ mount_status.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 # A script to check whether volumes are mounted with volume_name and filesystem_type as iinput variables
 
+debug_flag=0
+debug_file=/dev/null
+
 custom_plugin_dir=`dirname $0`
 source $custom_plugin_dir/common_things.sh
 volume_name=$1
-echo $volume_name
-filesystem_type=$2
-echo $filesystem_type
+[ $debug_flag -eq 0 ] && echo $volume_name
+filesystem_type=$2 
+[ $debug_flag -eq 0 ] && echo $filesystem_type
 grep_output=`mount | grep -e $volume_name | grep -e $filesystem_type`
 exit_status=`echo  $?`
-echo $grep_output
-echo $exit_status
+[ $debug_flag -eq 0 ] && echo $grep_output
+[ $debug_flag -eq 0 ] && echo $exit_status
+
 if [ $exit_status = "0" ]
 then
 	echo "$volume_name : is mounted"
